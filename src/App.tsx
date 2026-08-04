62e0b196d898d8971c2b6a463c98597872184c90
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-08-04 15:16:02 +1200

    remove unused h1 in app

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,6 @@ function App() {
   return (
     <>
       <div className="font-sans p-8 space-y-4">
-        <h1 className="text-3xl font-bold text-content-brand-primary">
-          Hello World
-        </h1>
         <Home />
         <CaseStudies />
         <Experience />

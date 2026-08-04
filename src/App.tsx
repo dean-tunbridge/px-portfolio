5323e5d326a743e09855453165d89aa578ca4fb4
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-08-04 18:17:25 +1200

    work on navbar.

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import Contact from './components/Contact'
 import Experience from './components/Experience'
 import Hero from './components/Hero'
 import Home from './components/Home'
+import Navbar from './components/Navbar'
 import Recommendations from './components/Recommendations'
 
 function App() {
   return (
     <>
-      <div className="font-sans p-8 space-y-4">
+      <div className=" font-sans p-8 space-y-4">
+        <Navbar />
         <Home />
         <Hero />
         <CaseStudies />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,52 @@
 export default function Navbar() {
-  return <div>Navbar</div>
+  return (
+    <div className="fixed top-0 right-0 left-0">
+      <nav className="p-2 gap-6 bg-surface-default-subtle border-b border-border-primary">
+        <ul className="space-6 text-base flex flex-row">
+          <li>
+            <a
+              className=" font-normal text-content-secondary px-2 py-1 rounded-md hover:bg-surface-default-subtle active:text-content-brand-secondary"
+              href="#">
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              className="font-normal text-content-secondary px-2 py-1 rounded-md hover:bg-surface-default-subtle active:text-content-brand-secondary"
+              href="#">
+              Case Studies
+            </a>
+          </li>
+          <li>
+            <a
+              className="font-normal text-content-secondary px-2 py-1 rounded-md hover:bg-surface-default-subtle active:text-content-brand-secondary"
+              href="#">
+              Experience
+            </a>
+          </li>
+          <li>
+            <a
+              className="font-normal text-content-secondary px-2 py-1 rounded-md hover:bg-surface-default-subtle active:text-content-brand-secondary"
+              href="#">
+              Recommendations
+            </a>
+          </li>
+          <li>
+            <a
+              className="font-normal text-content-secondary px-2 py-1 rounded-md hover:bg-surface-default-subtle active:text-content-brand-secondary"
+              href="#">
+              About
+            </a>
+          </li>
+          <li>
+            <a
+              className="font-normal text-content-secondary px-2 py-1 rounded-md hover:bg-surface-default-subtle active:text-content-brand-secondary"
+              href="#">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
 }

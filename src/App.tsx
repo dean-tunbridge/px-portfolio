b840a644dee50e8c98731bed79c5970cb2ed3c5d
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-08-04 18:35:09 +1200

    cleanup import in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
 import './App.css'
-import About from './components/About'
-import CaseStudies from './components/CaseStudies'
-import Contact from './components/Contact'
-import Experience from './components/Experience'
-import Hero from './components/Hero'
-import Home from './components/Home'
 import Navbar from './components/Navbar'
-import Recommendations from './components/Recommendations'
 
 function App() {
   return (
     <>
       <div className="font-sans">
         <Navbar />
-        <Home />
-        <Hero />
-        <CaseStudies />
-        <Experience />
-        <Recommendations />
-        <About />
-        <Contact />
       </div>
     </>
   )

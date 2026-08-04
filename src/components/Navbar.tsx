ab623746f872833c14ec6d1a4ef05e208abfb89a
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-08-04 18:24:40 +1200

    move navbar elements to center

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,8 @@
 export default function Navbar() {
   return (
-    <div className="fixed top-0 right-0 left-0">
-      <nav className="p-2 gap-6 bg-surface-default-subtle border-b border-border-primary">
-        <ul className="space-6 text-base flex flex-row">
+    <div className="fixed top-0 right-0 left-0 ">
+      <nav className="p-2 gap-6 bg-surface-default-subtle border-b border-border-primary ">
+        <ul className="space-6 text-base flex flex-row justify-center">
           <li>
             <a
               className="font-normal text-content-secondary px-2 py-1 rounded-md hover:bg-surface-default-subtle active:text-content-brand-secondary"
@@ -14,7 +14,7 @@ export default function Navbar() {
             <a
               className="font-normal text-content-secondary px-2 py-1 rounded-md hover:bg-surface-default-subtle active:text-content-brand-secondary"
               href="#">
-              Case Studies
+              Case studies
             </a>
           </li>
           <li>

acef8447e6c92d405dbdef24f1d8f0f6c32abc53
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-08-04 15:13:52 +1200

    import all components to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
 import './App.css'
+import About from './components/About'
+import CaseStudies from './components/CaseStudies'
+import Contact from './components/Contact'
+import Experience from './components/Experience'
+import Home from './components/Home'
+import Recommendations from './components/Recommendations'
 
 function App() {
   return (
@@ -7,6 +13,12 @@ function App() {
         <h1 className="text-3xl font-bold text-content-brand-primary">
           Hello World
         </h1>
+        <Home />
+        <CaseStudies />
+        <Experience />
+        <Recommendations />
+        <About />
+        <Contact />
       </div>
     </>
   )

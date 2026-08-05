f70e1f02e8c13bf254518191bc62edc34d083b39
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-08-05 18:29:22 +1200

    issue with navbar rendeering. Will troubleshoot now i have basic frame of component complete

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,54 +21,13 @@ export default function Navbar() {
             <li key={el.href}>
               <a
                 href={el.href}
-                label={el.label}
                 className="font-normal text-content-secondary px-2 py-1 rounded-md hover:bg-surface-default-subtle active:text-content-brand-secondary">
                 {el.label}
               </a>
             </li>
           ))}
-          {/* <li>
-            <a
-              className="font-normal text-content-brand-secondary px-2 py-1 rounded-md hover:bg-surface-default-subtle active:text-content-brand-secondary"
-              href="#">
-              Home
-            </a>
-          </li>
-          <li>
-            <a
-              className="font-normal text-content-secondary px-2 py-1 rounded-md hover:bg-surface-default-subtle active:text-content-brand-secondary"
-              href="#">
-              Case studies
-            </a>
-          </li>
-          <li>
-            <a
-              className="font-normal text-content-secondary px-2 py-1 rounded-md hover:bg-surface-default-subtle active:text-content-brand-secondary"
-              href="#">
-              Experience
-            </a>
-          </li>
-          <li>
-            <a
-              className="font-normal text-content-secondary px-2 py-1 rounded-md hover:bg-surface-default-subtle active:text-content-brand-secondary"
-              href="#">
-              Recommendations
-            </a>
-          </li>
-          <li>
-            <a
-              className="font-normal text-content-secondary px-2 py-1 rounded-md hover:bg-surface-default-subtle active:text-content-brand-secondary"
-              href="#">
-              About
-            </a>
-          </li>
-          <li>
-            <a
-              className="font-normal text-content-secondary px-2 py-1 rounded-md hover:bg-surface-default-subtle active:text-content-brand-secondary"
-              href="#">
-              Contact
-            </a>
-          </li> */}
+
+          {/* text-content-brand-secondary */}
         </ul>
       </nav>
     </div>

d896826f0aedecfe4ba833bc5eafb55269922eab
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-08-05 17:21:38 +1200

    wokring through errors in my designs. fix font colours for hero section, fix shadow syntax, and remove duplicate classes from tailwind classes throughout

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,26 +9,26 @@ export default function Hero() {
                  bg-amethyst-300
                 border-b-96 border-brick-500">
       <CardComponent size="base" colour="butterSubtle">
-        <div className="flex flex-col items-start gap-6 w-full">
-          <div className="flex flex-col items-start w-full gap-2">
-            <h1 className="font-sans font-black text-4xl leading-[1.6] w-full">
+        <div className="flex flex-col items-start gap-6 ">
+          <div className="flex flex-col items-start gap-2">
+            <h1 className="font-sans font-black text-4xl ">
               <span className="text-content-brand-primary">
-                Hello, I&apos;m{' '}
+                Hello, I&apos;m Priscilla
               </span>
-              <span className="text-content-brand-secondary">Priscilla</span>
+              {/* <span className="text-content-brand-secondary">Priscilla</span> */}
             </h1>
 
-            <p className="font-sans font-black text-4xl leading-[1.6] w-full">
-              <span className="text-content-brand-primary">
+            <p className="font-sans font-black text-4xl ">
+              <span className="text-content-brand-secondary">
                 a Product Owner turned{' '}
               </span>
-              <span className="text-content-brand-secondary [text-shadow:2px_2px_0_0_var(--color-butter-500)]">
+              <span className="text-content-brand-secondary text-shadow-sm text-shadow-butter-500">
                 UX Designer
               </span>
             </p>
           </div>
 
-          <p className="font-sans font-normal text-xl leading-[1.6] w-full">
+          <p className="font-sans font-normal text-xl">
             I am a bonson changing careers in a turbulent IT industry filled
             with techbrodudes and CEO dingleberries that are an absolute waste
             of space in humanity.

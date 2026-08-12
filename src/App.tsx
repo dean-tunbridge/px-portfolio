d9fe1d2bb6bb2c6c2ed88584b27fb2ad43b7aa99
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-08-13 06:30:02 +1200

    import CAse studies to begin building component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
+import CaseStudies from './components/CaseStudies'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       </header>
       <main>
         <Hero />
+        <CaseStudies />
       </main>
     </>
   )

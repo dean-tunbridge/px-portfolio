a615269f08da17898916f456be5725c8efb32970
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-08-13 07:28:26 +1200

    continue work on case study card. Will need t make a browser bar component to reuse on case study

--- src/components/CaseStudyCard.tsx
+++ src/components/CaseStudyCard.tsx
@@ -15,7 +15,12 @@ export default function CaseStudyCard({
 }: CaseStudyCardProps) {
   return (
     <div className="flex flex-row flex-wrap items-center content-center pr-6 gap-6 isolate w-full max-w-7xl">
-      <CardComponent size="small" colour={colour}></CardComponent>
+      <CardComponent
+        size="small"
+        colour={colour}
+        className="w-full max-w-154 min-w-85.5 z-1 flex flex-row justify-center items-center">
+        <div className="flex flex-col items-start w-full"></div>
+      </CardComponent>
     </div>
   )
 }

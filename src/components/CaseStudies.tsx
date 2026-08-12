79fa8c4669b43037ffd783d79d38b170e556afd9
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-08-13 06:44:18 +1200

    add image, adjust size based on specs.

--- src/components/CaseStudies.tsx
+++ src/components/CaseStudies.tsx
@@ -4,9 +4,11 @@ export default function CaseStudies() {
       className="box-border flex flex-col justify-center items-center
                  pt-46 px-0 pb-12
                  w-full h-136
+                 bg-[url(src/assets/px-pattern.png)]
+                 bg-repeat
+                 bg-size-[5%_5%]
                  bg-surface-default-subtle
-                border-border-primary">
-      CaseStudies
-    </div>
+                border-border-primary
+                border"></div>
   )
 }

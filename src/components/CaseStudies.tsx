23a4fe34f4885e024b740f2f696c6035963a0b19
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-08-13 06:39:01 +1200

    add bg colour and border colour to case studies component

--- src/components/CaseStudies.tsx
+++ src/components/CaseStudies.tsx
@@ -1,3 +1,12 @@
 export default function CaseStudies() {
-  return <div>CaseStudies</div>
+  return (
+    <div
+      className="box-border flex flex-col justify-center items-center
+                 pt-46 px-0 pb-12
+                 w-full h-136
+                 bg-surface-default-subtle
+                border-border-primary">
+      CaseStudies
+    </div>
+  )
 }

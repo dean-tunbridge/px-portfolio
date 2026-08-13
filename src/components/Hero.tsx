30c299f5e198b68c845601eec18a7f2f5c0f4160
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-08-13 13:08:09 +1200

    fix card size in hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,7 +10,7 @@ export default function Hero() {
                  bg-amethyst-300
                 border-b-96 border-brick-500">
       <CardComponent
-        size="large"
+        size="base"
         colour="butterSubtle"
         className="w-full max-w-(--width-max-content)">
         <div className="flex flex-col items-start gap-6">
